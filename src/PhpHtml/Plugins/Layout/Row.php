348d1503587f0133a89c81a88b4579c73322556a
André Moreira <andre.mcx1@gmail.com>
2018-11-26 04:20:32 -0300

    Row html

--- src/PhpHtml/Plugins/Layout/Row.php
+++ src/PhpHtml/Plugins/Layout/Row.php
@@ -9,7 +9,24 @@
 namespace PhpHtml\Plugins\Layout;
 
 
-class Row
-{
+use PhpHtml\Interfaces\PluginInterface;
 
+class Row implements PluginInterface
+{
+    private $columns = [];
+
+    public function col()
+    {
+        return $this->columns[] = app(Column::class);
+    }
+
+     public function getHtml(): string
+     {
+         $html = '';
+         foreach ($this->columns as $col) {
+             $html .= $col->getHtml();
+         }
+
+         return "<div class='row'>$html</div>";
+     }
 }
