7c0912e1b6e6b17729e4772fcfc943e0bba3753f
André Moreira <andre.mcx1@gmail.com>
2018-12-10 00:43:06 -0300

    Implementando funcionalidade de criar plugins dentro do atual

--- src/PhpHtml/Abstracts/PluginAbstract.php
+++ src/PhpHtml/Abstracts/PluginAbstract.php
@@ -7,8 +7,10 @@
 
 namespace PhpHtml\Abstracts;
 
-
+use PhpHtml\Errors\PluginNonexistentError;
 use PhpHtml\Interfaces\PluginInterface;
+use PhpHtml\Plugins\Col;
+use PhpHtml\Plugins\Row;
 
 abstract class PluginAbstract implements PluginInterface
 {
@@ -18,6 +20,56 @@ abstract class PluginAbstract implements PluginInterface
     protected $attributes = [];
 
     /**
+     * Referência da linha pai
+     *
+     * @var \PhpHtml\Plugins\Row
+     */
+    private $row;
+
+    /**
+     * Referência da coluna pai
+     *
+     * @var \PhpHtml\Plugins\Col
+     */
+    private $col;
+
+    private $items = [];
+
+    /**
+     * @return Row
+     */
+    public function getRow(): Row
+    {
+        return $this->row;
+    }
+
+    /**
+     * @param Row $row
+     */
+    public function setRow(Row $row): void
+    {
+        $this->row = $row;
+    }
+
+    /**
+     * @return Col
+     */
+    public function getCol(): Col
+    {
+        return $this->col;
+    }
+
+    /**
+     * @param Col $col
+     */
+    public function setCol(Col $col): void
+    {
+        $this->col = $col;
+    }
+
+    /**
+     * Define atributos de tag ou cria plugins dentro do atual
+     *
      * @param $name
      * @param $arguments
      * @return $this
@@ -25,14 +77,40 @@ abstract class PluginAbstract implements PluginInterface
      */
     public function __call($name, $arguments)
     {
+        $prefix = substr($name, 0, 3);
+        $suffix = substr($name, 3);
+
         throw_if(
-            !substr($name, 0, 3) == 'set',
+            !in_array($prefix, ['set', 'add']),
             \Exception::class,
             "Method $name don't exists!"
         );
 
-        $this->attributes[mb_strtolower(substr($name, 3))] = $arguments[0];
-        return $this;
+        if ($prefix == 'set') { // DEFINE ATRIBUTOS DE TAG
+            $this->attributes[mb_strtolower(substr($name, 3))] = $arguments[0];
+            return $this;
+        } elseif ($prefix == 'add') { // CRIA PLUGINS DENTRO DO ATUAL
+            $class = "PhpHtml\Plugins\\$suffix";
+
+            // LANÇA UM ERRO PERSONALIZADO SE O PLUGIN NÃO EXISTIR
+            try {
+                $obj = new $class(...$arguments);
+            } catch (\Error $e) {
+                throw new PluginNonexistentError("Plugin $class does not exist!");
+            }
+
+            return $this->items[] = $obj;
+        }
+    }
+
+    /**
+     * Retorna o vetor de plugins recebidos
+     *
+     * @return array
+     */
+    protected function getItems()
+    {
+        return $this->items;
     }
 
     /**
