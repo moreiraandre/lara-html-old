ce53bccde4a82d28735d90070342c640b433c842
André Moreira <andre.mcx1@gmail.com>
2018-12-14 14:35:38 -0300

    Exibindo erros nos exemplos

--- examples/index.php
+++ examples/index.php
@@ -1,4 +1,10 @@
-<?php require_once __DIR__ . '/../vendor/autoload.php'; ?>
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+?>
 
 <!doctype html>
 <html lang="pt-br">
