ef6be68d2752dbd31876c3a54e4bd8c0d26c28ad
André Moreira <andre.mcx1@gmail.com>
2019-09-01 23:00:18 -0400

    funcionando

--- src/Grid/Plugin.php
+++ src/Grid/Plugin.php
@@ -42,7 +42,7 @@ class Plugin extends General
      * @param array $arguments
      * @param string|null $template
      * @throws LaraHtmlPluginNotFoundException
-     * @throws \LaraHtml\Exceptions\LaraHtmlConfigNotFoundException
+     * @throws \LaraHtml\Exceptions\LaraHtmlConfigNotFoundException|\Exception
      */
     public function __construct(string $pluginName, array $arguments = [], string $template = null)
     {
@@ -58,99 +58,112 @@ class Plugin extends General
 
 //        PERCORRER OS VETORES DE CONFIGURAÇÃO DO PLUGIN E ARGUMENTOS, SOMENTE MANIPULANDO OS PONTEIROS INTERNOS DO ARRAY
 
+        $configIndex = 0; // ÍNDICE DA CONFIGURAÇÃO ATUAL
+        /*
+         * NA ATRIBUIÇÃO DE CONFIGURAÇÃO PELO CONSTRUTOR DA CLASSE, NÃO HAVERÁ CONCATENAÇÃO DE VALOR DE CONFIGURAÇÃO,
+         * APENAS SUBSTITUIÇÃO SE O VALOR FOR INFORMADO NO CONSTRUTOR OU UTILIZAÇÃO DO VALOR PRÉ-DEFINIDO NA CONFIGURAÇÃO
+         * SE O VALOR NÃO FOR PASSADO NO CONSTRUTOR.
+         */
         foreach ($configPlugin as $configPluginIndex => $configPluginValue) {
+            $configName = // NOME DA CONFIGURAÇÃO (ATRIBUTO OU META-DADO)
+            $configValue = null; // VALOR DA CONFIGURAÇÃO (ESTÁTICO OU INTERPRETADO PELA FUNÇÃO eval())
             /*
-             * $configPluginIndex:
-             * - Pode ser um "inteiro" (índice), quando apenas o nome da configuração é declarada, e não possui valor
-             *   pré-definido
-             * - Pode ser uma "cadeia de caracteres", quando o nome da configuração é declarada com um valor pré-definido
-             *   - Possui cadeia de caracteres "eval..": A cadeia de caracteres após deve ser interpretada como um comando
-             *     PHP através da função "eval()", gera exceção.
-             *   - Não possui cadeia de caracteres "eval..": O valor deve ser interpretado como caracteres estáticos.
+             * 1. $configPluginIndex:
+             *    1.1. Pode ser um "inteiro" (índice), quando apenas o nome da configuração é declarada, e não possui
+             *         valor pré-definido.
+             *         $configName = $configPluginValue;
+             *         $configValue = $arguments[$configIndex];
+             *         1.1.1. Se "meta." estiver contido em $configPluginValue
+             *                1.1.1.1. Se $arguments[$configIndex] existir: UM META-DADO SERÁ CRIADO!
+             *                1.1.1.2. Se $arguments[$configIndex] NÃO existir: Nenhuma configuração será definida!
+             *         1.1.2. Se "meta." NÃO estiver contido em $configPluginValue
+             *                1.1.2.1. Se $arguments[$configIndex] existir: UM ATRIBUTO SERÁ CRIADO!
+             *                1.1.2.2. Se $arguments[$configIndex] NÃO existir: Nenhuma configuração será definida!
+             *    1.2. Pode ser uma "cadeia de caracteres", quando o nome da configuração é declarada com um valor
+             *         pré-definido.
+             *         $configName = $configPluginIndex;
+             *         1.2.1. Se "meta." estiver contido em $configPluginIndex
+             *                1.2.1.1. Se $arguments[$configIndex] existir:
+             *                         $configValue = $arguments[$configIndex];
+             *                         UM META-DADO SERÁ CRIADO!
+             *                1.2.1.2. Se $arguments[$configIndex] NÃO existir:
+             *                         1.2.1.2.1. Se "eval.." estiver contido em $configPluginValue: A sequência de
+             *                                    caracteres posterior deve ser interpretada com a função "eval()".
+             *                                    UM META-DADO SERÁ CRIADO!
+             *                                    OU UMA EXCEÇÃO SERÁ GERADA!
+             *                         1.2.1.2.2. Se "eval.." NÃO estiver contido em $configPluginValue:
+             *                                    $configValue = $configPluginValue;
+             *                                    UM META-DADO SERÁ CRIADO!
+             *         1.2.2. Se "meta." NÃO estiver contido em $configPluginIndex
+             *                1.2.2.1. Se "eval.." estiver contido em $configPluginValue: A sequência de caracteres
+             *                           posterior deve ser interpretada com a função "eval()".
+             *                           UM ATRIBUTO SERÁ CRIADO!
+             *                           OU UMA EXCEÇÃO SERÁ GERADA!
+             *                1.2.2.2. Se "eval.." NÃO estiver contido em $configPluginValue:
+             *                           $configValue = $configPluginValue;
+             *                           UM ATRIBUTO SERÁ CRIADO!
              *----------------------------------------------------------------------------------------------------------
-             * $configPluginValue: Sempre será uma "cadeia de caracteres".
-             * - $configPluginIndex é "inteiro": $configPluginValue será o nome da configuração.
-             * - $configPluginIndex é uma "cadeia de caracteres": $configPluginValue será o valor pré-definido da
-             *   configuração.
+             * 2. $configPluginValue: Sempre será uma "cadeia de caracteres".
+             *    2.1. $configPluginIndex é "inteiro": $configPluginValue será o nome da configuração.
+             *    2.2. $configPluginIndex é uma "cadeia de caracteres": $configPluginValue será o valor pré-definido da
+             *         configuração (pode ser texto estático ou interpretado pela função "eval()").
+             *----------------------------------------------------------------------------------------------------------
+             * 3. $arguments: Vetor dos valores que serão atribuidos as configurações
+             *    3.1 Se $arguments[$configIndex] existir, o valor será da configuração atual.
              *
              */
-//            echo "<pre>";
-//            print_r([$configPluginIndex, $configPluginValue, gettype($configPluginIndex)]);
-//            print_r([$configPluginIndex, $configPluginValue, gettype($configPluginValue)]);
-//            print_r([$configPluginIndex]);
-//            print_r([$configPluginValue]);
-//            print_r([$configPlugin, $configPluginIndex, $configPluginValue]);
-//            print_r([$configPluginIndex, $configPluginValue]);
-//            echo "</pre>---";
-        }
+            // DEBUG
+            /*echo "<pre>";
+            print_r([$configPluginIndex, $configPluginValue, gettype($configPluginIndex), gettype($configPluginValue)]);
+            echo "</pre>---";*/
 
-        $countConfigPlugin = 0;
-        foreach ($configPlugin as $configPluginIndex => $configPluginValue) {
-            /*if ($countConfigPlugin == 2)*/
-//                dd($configPlugin, $configPluginIndex, $configPluginValue, $arguments[$countConfigPlugin], $arguments);
-
-//            if (isset($configPlugin[$arguments[$countConfigPlugin]])) { // SE O VALOR DA CONFIGURAÇÃO DO PLUGIN FOI INFORMADO COMO ARGUMENTO
-//            if (isset($configPlugin[$arguments[$countConfigPlugin]]) && isset($arguments[$countConfigPlugin])) { // SE O VALOR DA CONFIGURAÇÃO DO PLUGIN FOI INFORMADO COMO ARGUMENTO
-            if (isset($arguments[$countConfigPlugin])) { // SE O VALOR DA CONFIGURAÇÃO DO PLUGIN FOI INFORMADO COMO ARGUMENTO
-                if ($countConfigPlugin == 1)
-                    dd($configPlugin, $configPluginIndex, $configPluginValue, $arguments[$countConfigPlugin], $arguments, $countConfigPlugin, $configPlugin[$countConfigPlugin]);
-
-                if (isset($configPlugin[$arguments[$countConfigPlugin]])) {
-                    if (mb_strtolower(explode('.', $configPlugin[$arguments[$countConfigPlugin]])[0]) != 'meta') // SE NÃO FOR UM META DADO, ADICIONE UM ATRIBUTO
-                        $this->{"attr{$configPlugin[$countConfigPlugin]}"}($arguments[$countConfigPlugin]);
-                    else { // SENÃO, ADICIONE UM META DADO
-
-                        // META DADOS
-                        $metaName = explode('.', $configPlugin[$arguments[$countConfigPlugin]]);
-                        if (count($metaName) == 2) {
-                            $valueHelp = '';
-
-                            try {
-                                eval('$valueHelp = ' . pos($arguments) . ';');
-                            } catch (\Throwable $exception) {
-                                $valueHelp = pos($arguments);
-                            } finally {
-                                $this->addMeta($metaName[1], $valueHelp);
-                            }
-                        }
+            if (is_int($configPluginIndex)) { // 1.1
+                $configName = $configPluginValue;
+                if (strpos($configName,'meta.') !== false) { // 1.1.1
+                    if (isset($arguments[$configIndex])) { // 1.1.1.1
+                        $configName = explode('.', $configName)[1];
+                        $this->addMeta($configName, $arguments[$configIndex]);
+                    }
+                } else { // 1.1.2
+                    if (isset($arguments[$configIndex])) { // 1.1.2.1
+                        $this->{"attr{$configName}"}($arguments[$configIndex]);
                     }
                 }
 
-                next($arguments);
-            } else { // SE O VALOR DA CONFIGURAÇÃO DO PLUGIN "NÃO" FOI INFORMADO COMO ARGUMENTO
-                if (mb_strtolower(explode('.', $configPluginIndex)[0]) != 'meta') { // SE NÃO FOR UM META DADO, ADICIONE UM ATRIBUTO
-                    $valueHelp = '';
-
-                    try {
-                        eval('$valueHelp = ' . $configPluginValue . ';');
-                    } catch (\Throwable $exception) {
-                        $valueHelp = $configPluginValue;
-                    } finally {
-                        $this->{"attr{$configPluginIndex}"}($valueHelp);
-//                        $this->addMeta($metaName[1], $valueHelp);
+            } elseif (is_string($configPluginIndex)) { // 1.2
+                $configName = $configPluginIndex;
+                if (strpos($configName, 'meta.') !== false) { // 1.2.1
+                    $configName = explode('.', $configName)[1];
+                    if (isset($arguments[$configIndex])) { // 1.2.1.1
+                        $this->addMeta($configName, $arguments[$configIndex]);
+                    } else {
+                        if (strpos($configPluginValue,'eval..') !== false) { // 1.2.2.1
+                            $configValue = str_replace('eval..', '', $configPluginValue);
+                            $configValue = eval("$configValue;");
+                            $this->addMeta($configName, $configValue);
+                        } else { // 1.2.2.2
+                            $this->addMeta($configName, $configPluginValue);
+                        }
                     }
-                } else { // SENÃO, ADICIONE UM META DADO
-
-                    // META DADOS
-                    $metaName = explode('.', $configPluginIndex);
-                    if (count($metaName) == 2) {
-                        $valueHelp = '';
-
-                        try {
-                            eval('$valueHelp = ' . $configPluginValue . ';');
-                        } catch (\Throwable $exception) {
-                            $valueHelp = $configPluginValue;
-                        } finally {
-                            $this->addMeta($metaName[1], $valueHelp);
+                } else { // 1.1.2
+                    if (isset($arguments[$configIndex])) { // 1.1.2.1
+                        $this->{"attr{$configName}"}($arguments[$configIndex]);
+                    } else { // 1.1.2.2
+                        if (strpos($configPluginValue, 'eval..') !== false) { // 1.2.2.1
+                            $configValue = str_replace('eval..', '', $configPluginValue);
+                            $configValue = eval("$configValue;");
+                            $this->{"attr{$configName}"}($configValue);
+                        } else { // 1.2.2.2
+                            $this->{"attr{$configName}"}($configPluginValue);
                         }
                     }
                 }
+            } else {
+                throw new \Exception('$configPluginIndex é de um tipo não reconhecido! ('
+                    . gettype($configPluginIndex) . ')');
             }
-            $countConfigPlugin++;
-
+            $configIndex++;
         }
-        reset($arguments);
-
 
         $this->row();
     }
